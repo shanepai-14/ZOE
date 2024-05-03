afdea2783ebaeccea3ea133cb9fee035ab11ac0d
John Paul Quintana <jpquintana2024@gmail.com>
2024-05-03 11:05:04 +0800

    Update 2024_05_03_013638_create_payments_table.php

--- database/migrations/2024_05_03_013638_create_payments_table.php
+++ database/migrations/2024_05_03_013638_create_payments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('account_id')->constrained();
+            $table->foreignId('account_id')->constrained()->onDelete('cascade');
             $table->date('date');
             $table->bigInteger('ammount');
             $table->text('method');
