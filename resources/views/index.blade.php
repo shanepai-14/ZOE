2fd9cb23c78d60dc1092b0acaea9db83a146bda3
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-06-06 21:45:19 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -645,6 +645,7 @@
                   
                     <div class="col-span-6 sm:col-full">
                         <button class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" type="submit">Save all</button>
+                        <button id="closeCompanyBtn" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">Close</button>
                     </div>
                 </div>
             </form>
@@ -711,6 +712,12 @@ window.loadFile = function(event) {
                 $('#modalBackground').addClass('hidden')
                 $('#modalContainer').addClass('hidden')
             })
+            $('#closeCompanyBtn').click(function() {
+                $('#companyDetails').addClass('hidden')
+                $('#companyContainer').addClass('hidden')
+            })
+
+            
             $('#addNew').click(function() {
                 $('#modalBackground').removeClass('hidden')
                 $('#modalContainer').removeClass('hidden')
